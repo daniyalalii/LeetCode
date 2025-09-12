044d091880b443e71eee74543d49882f09425a92
Daniyal Ali <92244730+daniyalalii@users.noreply.github.com>
2025-09-12 09:58:22 +0500

    Time: 3 ms (87.13%) | Memory: 18.9 MB (79.48%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        for(auto i : s){
-            if(i =='a' || i=='e' || i == 'o' || i == 'i' || i == 'u'){
+        // for(auto i : s){
+        //     if(i =='a' || i=='e' || i == 'o' || i == 'i' || i == 'u'){
+        //         return true;
+        //     }
+        // }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'i' || s[i] == 'u') {
                 return true;
             }
         }
