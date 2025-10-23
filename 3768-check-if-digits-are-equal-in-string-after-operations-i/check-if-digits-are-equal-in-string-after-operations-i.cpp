de5d104b79776c10c8908ac6b6b8805ba59a43bc
Daniyal Ali <92244730+daniyalalii@users.noreply.github.com>
2025-10-23 14:44:48 +0500

    Time: 3 ms (87.54%) | Memory: 8.5 MB (85.52%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -3,7 +3,7 @@ public:
     bool hasSameDigits(string s) {
         for(int i = s.size();i>2;i--){
             for(int j = 0;j<i-1;j++){
-                s[j] = ((s[j]+ s[j+1] - 2*'0') %10) + '0';
+                s[j] = ((s[j]+ s[j+1] - '0') %10) + '0';
             }
         }
     return s[0] == s[1];
