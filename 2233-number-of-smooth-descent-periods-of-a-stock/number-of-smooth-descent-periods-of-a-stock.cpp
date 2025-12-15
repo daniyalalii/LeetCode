e1dc90daa03b4bf46de76886b461289c5525f0ca
Daniyal Ali <92244730+daniyalalii@users.noreply.github.com>
2025-12-15 10:37:52 +0500

    Time: 3 ms (41.64%) | Memory: 106.9 MB (97.03%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long length = 1;
+        long long total = 1;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]== prices[i-1]-1){
+                length++;
+                // cout<<length<<" "<<total<<endl;
+            }
+            else{
+                length = 1;
+            }
+                total = total + length;
+                // cout<<length<<" "<<total<<endl;
+        }
+        return total;
+    }
+};
