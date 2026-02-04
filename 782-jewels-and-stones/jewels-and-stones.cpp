c3f9ffe00c6f28fc50cdb98417a70a4bd3e00f93
Daniyal Ali <92244730+daniyalalii@users.noreply.github.com>
2026-02-04 21:47:23 +0500

    Time: 1 ms (21.48%) | Memory: 8.8 MB (22.76%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int val = 0;
-        for(int i=0;i<jewels.size();i++){
-            val += count(stones.begin(),stones.end(),jewels[i]);
+        // for(int i=0;i<jewels.size();i++){
+        //     val += count(stones.begin(),stones.end(),jewels[i]);
+        // }
+        // return val;
+        unordered_set<char> set(jewels.begin(),jewels.end());
+        for(char s : stones){
+            if(set.count(s)) val++;
         }
         return val;
     }
