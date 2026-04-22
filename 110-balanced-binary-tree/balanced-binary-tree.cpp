6aff3cfc85a7b003c139f6becbb143bcd5fc072c
Daniyal Ali <92244730+daniyalalii@users.noreply.github.com>
2026-04-22 16:48:16 +0500

    Time: 0 ms (100.00%) | Memory: 22.9 MB (82.50%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,11 +10,8 @@
  * };
  */
 class Solution {
-public:
-    bool isBalanced(TreeNode* root) {
-        return dfs(root) != -1;
-    }
-    int dfs(TreeNode* node){
+    private:
+        int dfs(TreeNode* node){
         if(!node) return false;
         int lHeight = dfs(node->left);
         if(lHeight==-1) return -1;
@@ -22,5 +19,9 @@ public:
         if(rHeight == -1) return -1;
         if(abs(lHeight - rHeight) >1) return -1;
         return 1 + max(lHeight,rHeight);
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
     }
 };
